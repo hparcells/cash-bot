f217ddc0922ed6e70675d08b1a40e17edb54a541
Hunter Parcells <hparcells@icloud.com>
2018-09-09 11:55:47 -0400

    Daily Cash

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,5 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ## [Unreleased]
 ### Additions
 - Ability to create an account and see your balance.
+- Ability to pay other people.
+- Ability to claim 20 cash everyday.
---
+++ modules/daily.mod.js
@@ -0,0 +1,45 @@
+const colors = require("colors");
+const fsn = require("fs-nextra");
+
+exports.id = "daily";
+
+let canClaim = true;
+
+exports.onLoad = api => {
+    api.commands.add("daily", (msg) => {        
+        fsn.readJSON("./accounts.json").then((accountDB) => {
+            let account = accountDB[msg.author.username.toLowerCase()];
+
+            // Checks if user account exists.
+            if(account !== undefined) {
+                // Checks if the time is right to claim.
+                if(canClaim) {
+                    canClaim = false;
+
+                    // Get cash.
+                    msg.reply("You claimed your daily 20 cash. You can get your next daily cash in 24 hours.");
+                    let amount = account.amount + 20;
+                    accountDB[msg.author.username.toLowerCase()] = {
+                        "owner": msg.author.id,
+                        "amount": amount
+                    }
+
+                    // Writes data to JSON.
+                    fsn.writeJSON("./accounts.json", accountDB, {
+                        replacer: null,
+                        spaces: 4
+                    });
+
+                    setTimeout(() => canClaim = true, 86400000);
+                }else {
+                    msg.reply(`You can not claim your daily cash. Wait .`);
+                }
+            }else {
+                msg.reply("You do not have an account, use `$new` to create a new account.");
+            }
+        });
+
+        // Logs in console.
+        console.log(colors.green(`${msg.author.username} used the daily command and got their daily cash.`));
+    })
+};
