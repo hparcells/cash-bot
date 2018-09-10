aa01d8db27c2ef9e53388fbf61c0bb55decfb21f
Hunter Parcells <hparcells@icloud.com>
2018-09-10 19:20:28 -0400

    Fixed FiftyFifty

--- modules/fiftyfifty.mod.js
+++ modules/fiftyfifty.mod.js
@@ -10,54 +10,70 @@ exports.onLoad = api => {
 
         fsn.readJSON("./accounts.json").then((accountDB) => {
             let account = accountDB[msg.author.username.toLowerCase()];
-            /* 
-                1 = Win
-                2 = Lose
-            */
-            let win = Math.floor(Math.random() * 2) + 1;
             
-            if(win === 1) {
-                let accountAfter = account.amount + bet;
-                
-                // Set JSON information.
-                accountDB[msg.author.username.toLowerCase()] = {
-                    "owner": msg.author.id,
-                    "amount": accountAfter,
-                    "lastClaimed": account.lastClaimed
+            // Checks if user account exists.
+            if(account !== undefined) {
+                // Checks if bet is negative.
+                if(bet > 0) {
+                    // Checks if user has enough money.
+                    if(bet <= account.amount) {
+                        /* 
+                            1 = Win
+                            2 = Lose
+                        */
+                        let win = Math.floor(Math.random() * 2) + 1;
+                        
+                        if(win === 1) {
+                            let accountAfter = account.amount + bet;
+                            
+                            // Set JSON information.
+                            accountDB[msg.author.username.toLowerCase()] = {
+                                "owner": msg.author.id,
+                                "amount": accountAfter,
+                                "lastClaimed": account.lastClaimed
+                            }
+    
+                            // Writes data to JSON.
+                            fsn.writeJSON("./accounts.json", accountDB, {
+                                replacer: null,
+                                spaces: 4
+                            }).then(() => {
+                                // Send message.
+                                msg.reply(`You won the 50/50 and got **${bet} Cash**`);
+    
+                                // Logs in console.
+                                console.log(colors.green(`${msg.author.username} used the 5050 command and won.`));
+                            });
+                        }else {
+                            let accountAfter = account.amount - bet;
+    
+                            // Set JSON information.
+                            accountDB[msg.author.username.toLowerCase()] = {
+                                "owner": msg.author.id,
+                                "amount": accountAfter,
+                                "lastClaimed": account.lastClaimed
+                            }
+    
+                            // Writes data to JSON.
+                            fsn.writeJSON("./accounts.json", accountDB, {
+                                replacer: null,
+                                spaces: 4
+                            }).then(() => {
+                                // Send message.
+                                msg.reply(`You lost the 50/50 and lost **${bet} Cash**`);
+    
+                                // Logs in console.
+                                console.log(colors.green(`${msg.author.username} used the 5050 command and lost.`));
+                            });
+                        }
+                    }else {
+                        msg.reply("You do not have enough Cash for that action.");
+                    }
+                }else {
+                    msg.reply("You cannot bet negative numbers nor 0.");
                 }
-
-                // Writes data to JSON.
-                fsn.writeJSON("./accounts.json", accountDB, {
-                    replacer: null,
-                    spaces: 4
-                }).then(() => {
-                    // Send message.
-                    msg.reply(`You won the 50/50 and got **${bet} Cash**`);
-
-                    // Logs in console.
-                    console.log(colors.green(`${msg.author.username} used the 5050 command and won.`));
-                });
             }else {
-                let accountAfter = account.amount - bet;
-
-                // Set JSON information.
-                accountDB[msg.author.username.toLowerCase()] = {
-                    "owner": msg.author.id,
-                    "amount": accountAfter,
-                    "lastClaimed": account.lastClaimed
-                }
-
-                // Writes data to JSON.
-                fsn.writeJSON("./accounts.json", accountDB, {
-                    replacer: null,
-                    spaces: 4
-                }).then(() => {
-                    // Send message.
-                    msg.reply(`You lost the 50/50 and lost **${bet} Cash**`);
-
-                    // Logs in console.
-                    console.log(colors.green(`${msg.author.username} used the 5050 command and lost.`));
-                });
+                msg.reply("You do not have an account, use `$new` to create a new account.");
             }
         });
     })
