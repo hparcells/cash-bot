d95e14438752a01dcb433c4172a0f4ce82eb85da
Hunter Parcells <hparcells@icloud.com>
2018-09-12 15:24:29 -0400

    Delete Checks for Non-Existent Accounts

--- modules/delete.mod.js
+++ modules/delete.mod.js
@@ -6,22 +6,28 @@ exports.id = "delete";
 exports.onLoad = api => {
     api.commands.add("delete", (msg) => {
         fsn.readJSON("./accounts.json").then((accountDB) => {
-            delete accountDB[msg.author.username.toLowerCase()];
+            let account = accountDB[msg.author.username.toLowerCase()];
 
-            // Writes data to JSON.
-            fsn.writeJSON("./accounts.json", accountDB, {
-                replacer: null,
-                spaces: 4
-            }).then(() => {
-                msg.reply(`You successfully deleted your account.`);
-
-                // Logs in console.
-                console.log(colors.green(`${msg.author.username} deleted their account.`));
-            }).catch((err) => {
-                if(err) {
-                    msg.reply(`There was an error while writing to the database! Show the following message to a developer: \`\`\`${err}\`\`\``);
-                }
-            });
+            if(account !== undefined) {
+                delete accountDB[msg.author.username.toLowerCase()];
+                
+                // Writes data to JSON.
+                fsn.writeJSON("./accounts.json", accountDB, {
+                    replacer: null,
+                    spaces: 4
+                }).then(() => {
+                    msg.reply(`You successfully deleted your account.`);
+                    
+                    // Logs in console.
+                    console.log(colors.green(`${msg.author.username} deleted their account.`));
+                }).catch((err) => {
+                    if(err) {
+                        msg.reply(`There was an error while writing to the database! Show the following message to a developer: \`\`\`${err}\`\`\``);
+                    }
+                });
+            }else {
+                msg.reply("You do not have an account to delete.");
+            }
         });
 
     })
