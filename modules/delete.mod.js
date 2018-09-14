dea812fbb0e1aeb6ab89d94ccc3a56413113b694
Hunter Parcells <hparcells@icloud.com>
2018-09-14 14:51:29 -0400

    Fixed Delete Abusing

--- modules/delete.mod.js
+++ modules/delete.mod.js
@@ -9,29 +9,43 @@ exports.onLoad = api => {
             let account = accountDB[msg.author.username.toLowerCase()];
 
             if(account !== undefined) {
-                delete accountDB[msg.author.username.toLowerCase()];
+                if(account.amount >= 50) {
+                    delete accountDB[msg.author.username.toLowerCase()];
                 
-                // Writes data to JSON.
-                fsn.writeJSON("./accounts.json", accountDB, {
-                    replacer: null,
-                    spaces: 4
-                }).then(() => {
+                    // Writes data to JSON.
+                    fsn.writeJSON("./accounts.json", accountDB, {
+                        replacer: null,
+                        spaces: 4
+                    }).then(() => {
+                        // Sends message.
+                        msg.channel.send({embed: {
+                            "title": ":x: Delete Account",
+                            "description": "You successfully deleted your account.",
+                            "thumbnail": {
+                                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Fxemoji_u274C.svg/1024px-Fxemoji_u274C.svg.png"
+                            }
+                        }});
+                        
+                        // Logs in console.
+                        console.log(colors.green(`${msg.author.username} deleted their account.`));
+                    }).catch((err) => {
+                        if(err) {
+                            msg.reply(`There was an error while writing to the database! Show the following message to a developer: \`\`\`${err}\`\`\``);
+                        }
+                    });
+                }else {
                     // Sends message.
                     msg.channel.send({embed: {
                         "title": ":x: Delete Account",
-                        "description": "You successfully deleted your account.",
+                        "description": "You do not have enough Cash to delete your account.",
                         "thumbnail": {
                             "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Fxemoji_u274C.svg/1024px-Fxemoji_u274C.svg.png"
                         }
                     }});
-                    
+
                     // Logs in console.
-                    console.log(colors.green(`${msg.author.username} deleted their account.`));
-                }).catch((err) => {
-                    if(err) {
-                        msg.reply(`There was an error while writing to the database! Show the following message to a developer: \`\`\`${err}\`\`\``);
-                    }
-                });
+                    console.log(colors.red(`${msg.author.username} didn't have enough Cash to delete their account.`));
+                }
             }else {
                 // Sends message.
                 msg.channel.send({embed: {
@@ -41,6 +55,9 @@ exports.onLoad = api => {
                         "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Fxemoji_u274C.svg/1024px-Fxemoji_u274C.svg.png"
                     }
                 }});
+
+                // Logs in console.
+                console.log(colors.red(`${msg.author.username} didn't have an account to delete.`));
             }
         });
     });
