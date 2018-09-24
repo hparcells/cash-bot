6851dd5c36bf19ee5ff6c6a1aa8025e1e2824d6c
Hunter Parcells <hparcells@icloud.com>
2018-09-24 15:06:02 -0400

    Database Getter Application

--- modules/delete.mod.js
+++ modules/delete.mod.js
@@ -6,53 +6,47 @@ const rethink = require("../index");
 exports.id = "delete";
 
 exports.onLoad = api => {
-    api.commands.add("delete", (msg) => {
-        r.table("accounts").get(msg.author.id).run(rethink.connection, async function(err, result) {
-            if(err) {
-                throw err;
-            }
+    api.commands.add("delete", async (msg) => {
+        if(await database.hasAccount(msg.author.id)) {
+            if(await database.getAmount(msg.author.id) >= 50) {
+                database.deleteAccount(msg.author.id);
 
-            if(await database.hasAccount(msg.author.id)) {
-                if(result.amount >= 50) {
-                    database.deleteAccount(msg.author.id);
-
-                    // Sends message.
-                    msg.channel.send({embed: {
-                        title: ":x: Delete Account",
-                        description: "You successfully deleted your account.",
-                        thumbnail: {
-                            url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Fxemoji_u274C.svg/1024px-Fxemoji_u274C.svg.png"
-                        }
-                    }});
-                    
-                    // Logs in console.
-                    console.log(colors.green(`${msg.author.username} deleted their account.`));
-                }else {
-                    // Sends message.
-                    msg.channel.send({embed: {
-                        title: ":x: Delete Account",
-                        description: "You do not have enough Cash to delete your account.",
-                        thumbnail: {
-                            url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Fxemoji_u274C.svg/1024px-Fxemoji_u274C.svg.png"
-                        }
-                    }});
-
-                    // Logs in console.
-                    console.log(colors.red(`${msg.author.username} didn't have enough Cash to delete their account.`));
-                }
-            }else {
                 // Sends message.
                 msg.channel.send({embed: {
                     title: ":x: Delete Account",
-                    description: "You do not have an account to delete.",
+                    description: "You successfully deleted your account.",
                     thumbnail: {
                         url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Fxemoji_u274C.svg/1024px-Fxemoji_u274C.svg.png"
                     }
                 }});
-    
+                
                 // Logs in console.
-                console.log(colors.red(`${msg.author.username} didn't have an account to delete.`));
+                console.log(colors.green(`${msg.author.username} deleted their account.`));
+            }else {
+                // Sends message.
+                msg.channel.send({embed: {
+                    title: ":x: Delete Account",
+                    description: "You do not have enough Cash to delete your account.",
+                    thumbnail: {
+                        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Fxemoji_u274C.svg/1024px-Fxemoji_u274C.svg.png"
+                    }
+                }});
+
+                // Logs in console.
+                console.log(colors.red(`${msg.author.username} didn't have enough Cash to delete their account.`));
             }
-        });
+        }else {
+            // Sends message.
+            msg.channel.send({embed: {
+                title: ":x: Delete Account",
+                description: "You do not have an account to delete.",
+                thumbnail: {
+                    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Fxemoji_u274C.svg/1024px-Fxemoji_u274C.svg.png"
+                }
+            }});
+
+            // Logs in console.
+            console.log(colors.red(`${msg.author.username} didn't have an account to delete.`));
+        }
     });
 };
--- modules/wallet.mod.js
+++ modules/wallet.mod.js
@@ -8,38 +8,32 @@ exports.id = "wallet";
 exports.onLoad = api => {
     api.commands.add("wallet", async (msg) => {
         if(await database.hasAccount(msg.author.id)) {
-            r.table("accounts").get(msg.author.id).run(rethink.connection, function(err, result) {
-                if(err) {
-                    throw err;
-                }
-    
-                let timeLeft = (result.lastClaimed + 86400000) - parseInt(Date.now());
-                let seconds = parseInt((timeLeft / 1000) % 60);
-                let minutes = parseInt((timeLeft / (1000 * 60)) % 60);
-                let hours = parseInt((timeLeft / (1000 * 60 * 60)) % 24);
+            let timeLeft = (database.getLastClaimed(msg.author.id) + 86400000) - parseInt(Date.now());
+            let seconds = parseInt((timeLeft / 1000) % 60);
+            let minutes = parseInt((timeLeft / (1000 * 60)) % 60);
+            let hours = parseInt((timeLeft / (1000 * 60 * 60)) % 24);
 
-                let stringTimeLeft = undefined;
-                if(Math.sign(timeLeft) === 1) {
-                    stringTimeLeft = `${hours} hours, ${minutes} minutes, and ${seconds} seconds.`;
-                }else {
-                    stringTimeLeft = "Ready to Claim!";
-                }
+            let stringTimeLeft = undefined;
+            if(Math.sign(timeLeft) === 1) {
+                stringTimeLeft = `${hours} hours, ${minutes} minutes, and ${seconds} seconds.`;
+            }else {
+                stringTimeLeft = "Ready to Claim!";
+            }
 
-                // Sends message.
-                msg.channel.send({embed: {
-                    title: ":moneybag: Your Wallet",
-                    fields: [{
-                        name: "Cash",
-                        value: `${result.amount} Cash`
-                    }, {
-                        name: "Time Until Next Daily",
-                        value: stringTimeLeft
-                    }],
-                    thumbnail: {
-                        url: "http://www.stickpng.com/assets/images/580b585b2edbce24c47b2878.png"
-                    }
-                }});
-            });
+            // Sends message.
+            msg.channel.send({embed: {
+                title: ":moneybag: Your Wallet",
+                fields: [{
+                    name: "Cash",
+                    value: `${await database.getAmount(msg.author.id)} Cash`
+                }, {
+                    name: "Time Until Next Daily",
+                    value: stringTimeLeft
+                }],
+                thumbnail: {
+                    url: "http://www.stickpng.com/assets/images/580b585b2edbce24c47b2878.png"
+                }
+            }});
 
             // Logs in console.
             console.log(colors.green(`${msg.author.username} used the wallet command.`));
